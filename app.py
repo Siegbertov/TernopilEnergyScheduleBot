81055f1cb469cb94a059f88c41a2a2ab0da08e35
Siegbertov <siegbertov@gmail.com>
2024-07-23 17:07:27 +0300

    Added /update implementation

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 import telebot
 from telebot import types
+from utils import scrapper, get_today_date_name, get_tomorrow_date_name
 from utils import create_users_table, __is_user_id_in_database, __add_user
+from utils import create_days_table, get_all_days_from_database, get_last_day_from_database, __add_day_to_database
 
 
 def configure():
@@ -14,17 +16,66 @@ if __name__ == "__main__":
     DATABASE_FILENAME = "small_db.sql"
     TOKEN = os.getenv('TOKEN')
     MY_USER_ID = os.getenv('MY_USER_ID')
+    LINK = os.getenv('LINK')
     bot = telebot.TeleBot(token=TOKEN)
 
     @bot.message_handler(commands=['start'])
     def start(message):
         create_users_table(db_filename=DATABASE_FILENAME)
+        create_days_table(db_filename=DATABASE_FILENAME)
         if not __is_user_id_in_database(db_filename=DATABASE_FILENAME, user_id=message.from_user.id):
             __add_user(db_filename=DATABASE_FILENAME, user_id=message.from_user.id)
             bot.send_message(message.chat.id, f"ADDED USER <{message.from_user.id}>") # FIXME edit later
         else:
             bot.send_message(message.chat.id, f"USER <{message.from_user.id}> ALREADY EXISTS") # FIXME edit later
-        
+    
+    @bot.message_handler(commands=['update'])
+    def update(message):
+        if message.from_user.id == MY_USER_ID:
+            DAYS = scrapper(link=LINK, day_month_r=r"(\d+) (\w+),", group_r=r"(\d\d:\d\d)-(\d\d:\d\d)\s+(\d)\s+\w+")
+            for day_name, groups in DAYS.items():
+                __add_day_to_database(db_filename=DATABASE_FILENAME, day_name=day_name, groups=groups)
+                bot.send_message(message.chat.id, f"UPDATED: <{day_name}>") # FIXME edit later
+        else:
+            bot.send_message(message.chat.id, f"This command only for admins") # FIXME edit later
+
+    @bot.message_handler(commands=['settings'])
+    def settings(message):
+        # TODO implement command /settings
+        bot.send_message(message.chat.id, "Not Implemented....yet!")
+
+    @bot.message_handler(commands=['get_today'])
+    def get_today(message):
+        # TODO implement command /get_today
+        txt = get_today_date_name()
+        bot.send_message(message.chat.id, txt)
+        bot.send_message(message.chat.id, "Not Implemented....yet!")
+    
+    @bot.message_handler(commands=['get_tomorrow'])
+    def get_tomorrow(message):
+        # TODO implement command /get_tomorrow
+        txt = get_tomorrow_date_name()
+        bot.send_message(message.chat.id, txt)
+        bot.send_message(message.chat.id, "Not Implemented....yet!")    
+
+    @bot.message_handler(commands=['get_last_available'])
+    def get_last_available(message):
+        db_day = get_last_day_from_database(db_filename=DATABASE_FILENAME)
+        d_n, g1, g2, g3, g4, g5, g6 = db_day
+        g_s = [g1, g2, g3, g4, g5, g6]
+        txt = f"*Графік на {d_n}:*"
+        for g in g_s:
+            txt = f"{txt} {g}"
+        txt = txt.replace("+", "\\+")
+        txt = txt.replace("-", "\\-")
+        bot.send_message(message.chat.id, txt, parse_mode='MarkdownV2') # FIXME edit later
+
+    # @bot.message_handler(commands=['get_all'])
+    # def get_all(message):
+    #     db_days = get_all_days_from_database(db_filename=DATABASE_FILENAME)
+    #     for db_day in db_days:
+    #         txt = " ".join(db_day)
+    #         bot.send_message(message.chat.id, txt) # FIXME edit later
 
     # END
     bot.polling(non_stop=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,37 @@
 import sqlite3
+import requests
+import re
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
+def __parse_date_as_ukr(date:str)->str:
+    MONTH_UA_OR = {
+                            "01":"січня",
+                            "02": "лютого",
+                            "03":"березня",
+                            "04": "квітня",
+                            "05":"травня",
+                            "06": "червня",
+                            "07":"липня",
+                            "08": "серпня",
+                            "09":"вересня",
+                            "10": "жовтня",
+                            "11":"листопада",
+                            "12": "грудня"
+                        }
+    day, month = date.split(" ")
+    return f"{day} {MONTH_UA_OR[month]}"
+
+def get_today_date_name()->str:
+    some_day = datetime.today().strftime("%d %m")
+    return __parse_date_as_ukr(some_day)
+
+def get_tomorrow_date_name()->str:
+    some_day = (datetime.today() + timedelta(days=1)).strftime("%d %m")
+    return __parse_date_as_ukr(some_day) 
+
+
+# FUNCTIONS FOR DATABASE <USERS>
 def create_users_table(db_filename:str)->None:
     connection = sqlite3.connect(db_filename)
     cursor = connection.cursor()
@@ -27,7 +59,7 @@ def __is_user_id_in_database(db_filename:str, user_id:str)->bool:
     cursor = connection.cursor()
 
     result = bool(cursor.execute(f"SELECT * FROM users WHERE user_id=:user_id", {'user_id':user_id}).fetchone())
-    
+
     connection.commit()
     cursor.close()
     connection.close()
@@ -43,12 +75,153 @@ def __add_user(db_filename:str, user_id:str):
     cursor.close()
     connection.close()  
 
-def add_user_if_not_exits(db_filename:str, user_id:str)->None:
-    if not __is_user_id_in_database(db_filename=db_filename, user_id=user_id):
-        __add_user(db_filename=db_filename, user_id=user_id)
+# FUNCTIONS FOR DATABASE <DAYS>
+def create_days_table(db_filename:str)->None:
+    connection = sqlite3.connect(db_filename)
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS days (
+                                day_name TEXT,
+                                first_g TEXT,
+                                second_g TEXT,
+                                third_g TEXT,
+                                fouth_g TEXT,
+                                fifth_g TEXT,
+                                sixth_g TEXT
+                            )""")
+    connection.commit()
+    cursor.close()
+    connection.close()
 
+def get_all_days_from_database(db_filename:str) ->list:
+    result = None
+    connection = sqlite3.connect(db_filename)
+    cursor = connection.cursor()
 
+    result = cursor.execute(f"SELECT * FROM days").fetchall()
 
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return result
+
+def get_last_day_from_database(db_filename:str) ->tuple:
+    return get_all_days_from_database(db_filename=db_filename)[-1]
+
+def __get_day_from_database(db_filename:str, day_name:str) -> tuple:
+    result = None
+    connection = sqlite3.connect(db_filename)
+    cursor = connection.cursor()
+
+    result = cursor.execute(f"SELECT * FROM days WHERE day_name=:day_name", {'day_name':day_name}).fetchone()
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return result
+
+def __is_day_in_database(db_filename:str, day_name:str)->bool:
+    return bool(__get_day_from_database(db_filename=db_filename, day_name=day_name))
+
+def __add_day_to_database(db_filename:str, day_name:str, groups:dict) ->None:
+    # TODO IMPLEMENT 
+    if __is_day_in_database(db_filename=db_filename, day_name=day_name):
+        db_day_tuple = __get_day_from_database(db_filename=db_filename, day_name=day_name)
+        parsed_day = [day_name]
+        for group in groups.values():
+            parsed_day.append(group)
+        parsed_day_tuple = tuple(parsed_day)
+        if not (db_day_tuple == parsed_day_tuple):
+            # TODO DAY IN DB + DIFF DAYS
+            connection = sqlite3.connect(db_filename)
+            cursor = connection.cursor()
+            cursor.execute(
+                    """UPDATE days SET first_g=:first_g, second_g=:second_g, third_g=:third_g,
+                    fouth_g=:fouth_g, fifth_g=:fifth_g, sixth_g=:sixth_g WHERE day_name=:day_name""",
+                    {'day_name': day_name, 'first_g': groups['1'], 'second_g': groups['2'], 'third_g': groups['3'], 
+                    'fouth_g': groups['4'], 'fifth_g': groups['5'], 'sixth_g': groups['6']}
+                                    )
+            connection.commit()
+            cursor.close()
+            connection.close()
+        else:
+            pass
+    else:
+        # TODO DAY NOT IN DB
+        connection = sqlite3.connect(db_filename)
+        cursor = connection.cursor()
+        cursor.execute(
+                    f"INSERT INTO days VALUES (:day_name, :first_g, :second_g, :third_g, :fouth_g, :fifth_g, :sixth_g)", 
+                    {'day_name': day_name, 'first_g': groups['1'], 'second_g': groups['2'], 'third_g': groups['3'], 
+                    'fouth_g': groups['4'], 'fifth_g': groups['5'], 'sixth_g': groups['6']}
+                                    ) 
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+# FUNCTIONS FOR SCRAPPING
+def __re_search_all_off_lines(text:str, pattern_r:str) -> dict:
+    result = {}
+    try:
+        pattern = re.compile(pattern_r)
+        matches = pattern.finditer(text)
+        for match in matches:
+            h1, h2, group = match.group(1), match.group(2), match.group(3)
+            if group not in result:
+                result[group] = []
+            result[group].append(f"{h1}-{h2}")
+    except Exception as e:
+        pass #TODO exception handling
+
+    for pair in result.keys():
+        result[pair].sort()
+
+    result = {k: v for k, v in sorted(list(result.items()))}
+
+    temp = {}
+    for pair in result.keys():
+        current_inline = "+".join(result[pair])
+        if not current_inline.startswith("00:00"):
+            current_inline = f"00:00+{current_inline}"
+        if not current_inline.endswith("24:00"):
+            current_inline = f"{current_inline}+24:00"
+        temp[pair] = current_inline
+
+    return temp
+
+def __re_search_day_and_month(text:str, pattern_r:str) -> tuple:
+    result = (None, None)
+    try:
+        pattern = re.compile(pattern_r)
+        match = pattern.search(text)
+        result = (match.group(1), match.group(2))
+    except Exception as e:
+        pass #TODO exception handling
+    return result
+
+def scrapper(link:str, day_month_r:str, group_r:str) -> dict:
+    result_d = {}
+    temp_d = {}
+    try:
+       r = requests.get(link)
+       soup = BeautifulSoup(r.text, 'html.parser')
+       posts = soup.find_all("div", {"class": "tgme_widget_message_bubble"})
+       for post in posts:
+            text = post.find("div", {"class": "tgme_widget_message_text"})
+
+            if text is not None:
+                day, month = __re_search_day_and_month(text=text.text, pattern_r=day_month_r)
+                groups = __re_search_all_off_lines(text=text.text, pattern_r=group_r)
+                if not(day is None or month is None or groups == {}):
+                    temp_d[f"{day} {month}"] = groups
+    except requests.ConnectionError as e:
+        pass
+
+    for reversed_key in list(temp_d.keys())[::-1]:
+        if reversed_key not in result_d:
+            result_d[reversed_key] = temp_d[reversed_key]
+    return dict(reversed(list(result_d.items())))
 
 
 
